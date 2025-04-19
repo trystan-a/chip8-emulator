57f763428c1f863a8af4ead63419da34970a52bf
Trystan Allen <tallen06@uoguelph.ca>
2025-04-19 19:04:50 -0400

    Got SDL3 working for my environment and put time into learning how to use it.

--- src/main.c
+++ src/main.c
@@ -1,8 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "chip8.h"
+#include "../include/chip8.h"
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
 
+// compile with this command for now:
+// gcc -Wall main.c -o chip8 -lSDL3
 int main(int agrc, char** argv) {
+    SDL_Window *window;
 
+    SDL_Init(SDL_INIT_VIDEO);
+    window = SDL_CreateWindow("Chip8 Emulator", 640, 320, 0);
+
+    if(window == NULL) {
+        printf("Error creating window");
+        return 1;
+    }
     
+    //game loop here
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return 0;
 }
